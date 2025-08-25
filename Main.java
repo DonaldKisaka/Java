dae7d052d3cd0f8c302881ffbc716c98d74b11d9
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-25 20:51:38 +0300

    Do while

--- Main.java
+++ Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      for (int i = 0; i < 5; i++) 
-        System.out.println("Hello world");
         
+      Scanner scanner = new Scanner(System.in);
+      String input = "";
+      while (!input.equals("quit")) {
+        System.out.print("Input: ");
+        input = scanner.next().toLowerCase();
+        System.out.println(input);
+      }
     }
 }
