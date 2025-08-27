17bf1dfae64e54f2a1b4423e2bc52bfced30cc25
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-27 21:39:43 +0300

    polymorphism

--- Main.java
+++ Main.java
@@ -1,17 +1,29 @@
-class Vehicle {
-   protected String brand = "Ford";
-   public void honk() {
-      System.out.println("Tuut, tuut!");
+class Animal {
+   public void animalSound() {
+      System.out.println("The animal makes a sound");
    }
 }
 
-class Car extends Vehicle {
-   private String modelName = "Mustang";
-   public static void main(String[] args) {
-      Car myCar = new Car();
-      
-      myCar.honk();
+class Pig extends Animal {
+   public void animalSound() {
+      System.out.println("The pig says: wee wee");
+   }  
+}
 
-      System.out.println(myCar.brand + " " + myCar.modelName);
+class Dog extends Animal {
+   public void animalSound() {
+      System.out.println("The dog says: bow wow");
+   }
+}
+
+class Main {
+   public static void main(String[] args) {
+      Animal myAnimal = new Animal();
+      Animal myPig = new Pig();
+      Animal myDog = new Dog();
+
+      myAnimal.animalSound();
+      myPig.animalSound();
+      myDog.animalSound();
    }
 }
