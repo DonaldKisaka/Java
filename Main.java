6cee22cca3824e3f1149dcb9694453cd8daee666
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-28 09:34:10 +0300

    enums

--- Main.java
+++ Main.java
@@ -1,21 +1,13 @@
-interface Animal {
-   public void animalSound();
-   public void sleep();
-}
-
-class Pig implements Animal {
-   public void animalSound() {
-      System.out.println("The pig says: wee wee");
+public class Main {
+   enum Level {
+      LOW,
+      MEDIUM,
+      HIGH
    }
-   public void sleep() {
-      System.out.println("Zzz");
-   }
-}
 
-class Main {
+
    public static void main(String[] args) {
-      Pig myPig = new Pig();
-      myPig.animalSound();
-      myPig.sleep();
+      Level myVar = Level.MEDIUM;
+      System.out.println(myVar);
    }
 }
